914f1305e8c085b85c706a53d3b13e800c43e7ba
Yonatan Kogan <yjkogan@gmail.com>
2012-07-02 15:22:31 -0700

    Initial Commit

---
+++ AsteroidsTests/AsteroidsTests.h
@@ -0,0 +1,13 @@
+//
+//  AsteroidsTests.h
+//  AsteroidsTests
+//
+//  Created by Romotive on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AsteroidsTests : SenTestCase
+
+@end
---
+++ AsteroidsTests/AsteroidsTests.m
@@ -0,0 +1,32 @@
+//
+//  AsteroidsTests.m
+//  AsteroidsTests
+//
+//  Created by Romotive on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AsteroidsTests.h"
+
+@implementation AsteroidsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AsteroidsTests");
+}
+
+@end
